f8a836379d852862339338c5dca224c33168ff3c
Furkan Ayhan <furkan197@outlook.de>
2023-02-21 16:21:41 +0100

    new categorie function + improved code

--- script.js
+++ script.js
@@ -1,64 +1,65 @@
 const questions = [
     {
-       "question":"Wer hat HTML erfunden?",
-       "answer_1":"Robbie Williams",
-       "answer_2":"Lady Gaga",
-       "answer_3":"Tim Berners-Lee",
-       "answer_4":"Justin Bieber",
-       "right_answer":3
+       "question":["Wer hat HTML erfunden?", "Was ist der Zweck von CSS-Selektoren?"],
+       "answer_1":["Robbie Williams", "Zum Auswählen von HTML-Elementen"],
+       "answer_2":["Lady Gaga", "Zum Formatieren von Text"],
+       "answer_3":["Tim Berners-Lee", "Zum Ändern des Verhaltens von Webseiten"],
+       "answer_4":["Justin Bieber", "Zum Erstellen von Animationen"],
+       "right_answer":[3, 1]
     },
     {
-       "question":"Was ist der Zweck des (alt) Attributs in einem (img) Tag?",
-       "answer_1":"Es gibt den Titel des Bildes an",
-       "answer_2":"Es beschreibt den Inhalt des Bildes für Nutzer, die das Bild nicht sehen können",
-       "answer_3":"Es gibt die Abmessungen des Bildes an",
-       "answer_4":"Es legt fest, ob das Bild horizontal oder vertikal ausgerichtet ist",
-       "right_answer":2
+       "question":["Was ist der Zweck des (alt) Attributs in einem (img) Tag?", "Was ist ein Pseudo-Element in CSS?"],
+       "answer_1":["Es gibt den Titel des Bildes an", "Ein Element, das auf bestimmte Benutzerinteraktionen wie den Hover-Effekt reagiert."],
+       "answer_2":["Es beschreibt den Inhalt des Bildes für Nutzer, die das Bild nicht sehen können", "Ein Element, das durch JavaScript generiert wird."],
+       "answer_3":["Es gibt die Abmessungen des Bildes an", "Ein Element, das den Inhalt eines anderen Elements anzeigt."],
+       "answer_4":["Es legt fest, ob das Bild horizontal oder vertikal ausgerichtet ist", "Ein Element, das spezielle Effekte wie Schatten oder Hintergrundbilder auf Elemente anwendet."],
+       "right_answer":[2, 3]
     },
     {
-       "question":"Was ist der Zweck des (head) Elements in HTML??",
-       "answer_1":"Es definiert den Hauptinhalt der Webseite",
-       "answer_2":"Es definiert den Fußbereich der Webseite",
-       "answer_3":"Es definiert die Navigationsleiste der Webseite",
-       "answer_4":"Es enthält Metadaten über die Webseite, wie z.B. den Titel, die Beschreibung oder die Codierung",
-       "right_answer":4
+       "question":["Was ist der Zweck des (head) Elements in HTML??", "Was ist der Zweck von Media Queries in CSS?"],
+       "answer_1":["Es definiert den Hauptinhalt der Webseite", "Zum Verstecken von Elementen auf bestimmten Bildschirmgrößen"],
+       "answer_2":["Es definiert den Fußbereich der Webseite", "Zum Ändern von Stylesheets auf verschiedenen Bildschirmgrößen"],
+       "answer_3":["Es definiert die Navigationsleiste der Webseite", "Zum Ändern von Stylesheets basierend auf dem Betriebssystem des Benutzers"],
+       "answer_4":["Es enthält Metadaten über die Webseite, wie z.B. den Titel, die Beschreibung oder die Codierung", "Zum Ändern von Stylesheets basierend auf dem Browser des Benutzers"],
+       "right_answer":[4, 2]
     },
     {
-       "question":"Was ist der Zweck des (href) Attributs in einem (a) Tag?",
-       "answer_1":"Es gibt den Text an, der beim Klick auf den Link angezeigt wird",
-       "answer_2":"Es gibt den Namen des Links an",
-       "answer_3":"Es gibt den Titel des Links an",
-       "answer_4":"Es gibt den Ziel-URL an, auf den der Link verweist",
-       "right_answer":4
+       "question":["Was ist der Zweck des (href) Attributs in einem (a) Tag?", "Was ist das Box-Modell in CSS?"],
+       "answer_1":["Es gibt den Text an, der beim Klick auf den Link angezeigt wird", "Eine Möglichkeit, verschiedene Layout-Optionen für ein Element anzupassen"],
+       "answer_2":["Es gibt den Namen des Links an", "Eine Möglichkeit, die Größe von Elementen relativ zu ihrer Eltern-Container-Größe anzugeben."],
+       "answer_3":["Es gibt den Titel des Links an", "Eine Methode zur Berechnung der Größe von HTML-Elementen, die aus Inhalt, Padding, Border und Margin besteht."],
+       "answer_4":["Es gibt den Ziel-URL an, auf den der Link verweist", "Eine Möglichkeit, ein Element in einem Webbrowser zu positionieren."],
+       "right_answer":[4, 3]
     },
     {
-       "question":"Was ist der Zweck des (form) Elements in HTML??",
-       "answer_1":"Es definiert den Inhalt der Webseite",
-       "answer_2":"Es definiert den Hauptbereich der Webseite",
-       "answer_3":"Es definiert ein Formular, in dem der Nutzer Eingaben tätigen kann",
-       "answer_4":"Es definiert eine Tabelle mit Daten",
-       "right_answer":3
+       "question":["Was ist der Zweck des (form) Elements in HTML??", "Was ist der Zweck von (float) in CSS?"],
+       "answer_1":["Es definiert den Inhalt der Webseite", "Zum Positionieren von Elementen relativ zu ihrer Eltern-Container-Größe"],
+       "answer_2":["Es definiert den Hauptbereich der Webseite", "Zum Anpassen der Größe von Elementen relativ zu ihrem Inhalt."],
+       "answer_3":["Es definiert ein Formular, in dem der Nutzer Eingaben tätigen kann", "Zum Hinzufügen von Hintergrundfarben zu Text."],
+       "answer_4":["Es definiert eine Tabelle mit Daten", "Zum Verschieben von Elementen zu einem bestimmten Bereich innerhalb ihres Containers."],
+       "right_answer":[3, 4]
     },
     {
-       "question":"Was ist der Zweck des (title) Attributs in einem (a) Tag?",
-       "answer_1":"Es gibt den Text an, der beim Klick auf den Link angezeigt wird",
-       "answer_2":"Es gibt den Namen des Links an",
-       "answer_3":"Es gibt den Ziel-URL an, auf den der Link verweist",
-       "answer_4":"Es gibt den Titel des Links an",
-       "right_answer":4
+       "question":["Was ist der Zweck des (title) Attributs in einem (a) Tag?", "Was ist der Zweck von (z-index) in CSS?"],
+       "answer_1":["Es gibt den Text an, der beim Klick auf den Link angezeigt wird", "Zum Einstellen der Transparenz eines Elements"],
+       "answer_2":["Es gibt den Namen des Links an", "Zum Einstellen der Größe eines Elements relativ zu seinem Container"],
+       "answer_3":["Es gibt den Ziel-URL an, auf den der Link verweist", "Zum Einstellen der Reihenfolge der Elemente in Bezug auf die Schichtung auf der Webseite"],
+       "answer_4":["Es gibt den Titel des Links an", "Zum Einstellen der Schriftart eines Elements"],
+       "right_answer":[4, 3]
     },
     {
-       "question":"Was ist der Zweck der HTML-Auszeichnungssprache?",
-       "answer_1":"Es definiert eine Tabelle mit Daten",
-       "answer_2":"Sie beschreibt, wie die Inhalte auf der Webseite angezeigt werden sollen",
-       "answer_3":"Sie ermöglicht es, interaktive Elemente auf der Webseite einzubinden",
-       "answer_4":"Sie sorgt dafür, dass die Webseite auf allen Geräten und in allen Browsern gleich dargestellt wird",
-       "right_answer":2
+       "question":["Was ist der Zweck der HTML-Auszeichnungssprache?", "Welche CSS-Eigenschaft wird verwendet, um den Hintergrund eines Elements zu definieren?"],
+       "answer_1":["Es definiert eine Tabelle mit Daten", "background-color"],
+       "answer_2":["Sie beschreibt, wie die Inhalte auf der Webseite angezeigt werden sollen", "text-color"],
+       "answer_3":["Sie ermöglicht es, interaktive Elemente auf der Webseite einzubinden", "border-color"],
+       "answer_4":["Sie sorgt dafür, dass die Webseite auf allen Geräten und in allen Browsern gleich dargestellt wird", "font-color"],
+       "right_answer":[2, 1]
     }
  ];
 
  
  let currentQuestion = 0;
+ let currentCategorie = 0;
  let shownQuestionNumber = 1;
  let correctAnswerAmount = 0;
  let AUDIO_SUCCESS = new Audio('audio/success.mp3');
@@ -74,11 +75,11 @@ const questions = [
 
  function showQuestion(){
     let question = questions[currentQuestion];
-    document.getElementById('questionText').innerHTML = question['question'];
-    document.getElementById('answer_1').innerHTML = `<div class="question-letter"><b>A</b></div>` + question['answer_1'];
-    document.getElementById('answer_2').innerHTML = `<div class="question-letter"><b>B</b></div>` + question['answer_2'];
-    document.getElementById('answer_3').innerHTML = `<div class="question-letter"><b>C</b></div>` + question['answer_3'];
-    document.getElementById('answer_4').innerHTML = `<div class="question-letter"><b>D</b></div>` + question['answer_4'];
+    document.getElementById('questionText').innerHTML = question['question'][currentCategorie];
+    document.getElementById('answer_1').innerHTML = `<div class="question-letter"><b>A</b></div>` + question['answer_1'][currentCategorie];
+    document.getElementById('answer_2').innerHTML = `<div class="question-letter"><b>B</b></div>` + question['answer_2'][currentCategorie];
+    document.getElementById('answer_3').innerHTML = `<div class="question-letter"><b>C</b></div>` + question['answer_3'][currentCategorie];
+    document.getElementById('answer_4').innerHTML = `<div class="question-letter"><b>D</b></div>` + question['answer_4'][currentCategorie];
     checkIfLastQuestion();
  }
 
@@ -92,30 +93,49 @@ const questions = [
 
  
  function answer(selection){
-   let correctAnswer = questions[currentQuestion]['right_answer']
+   let correctAnswer = questions[currentQuestion]['right_answer'][currentCategorie]
    let selectedAnswerNumber = selection.slice(-1);
-   let idOfRightAnswer = `answer_${questions[currentQuestion]['right_answer']}`
+   let idOfRightAnswer = `answer_${questions[currentQuestion]['right_answer'][currentCategorie]}`
    
-   if (selectedAnswerNumber == correctAnswer){     /* check if answer is correct */
+   if (selectedAnswerNumber == correctAnswer){     /* checks if answer is correct */
       answerIsCorrect(selection);
    } else {
       wrongAnswer(selection, idOfRightAnswer);
    }
    document.getElementById('next-button').disabled = false;
  }
-
+ 
 
  function answerIsCorrect(selection){
    document.getElementById(selection).parentNode.classList.add("bg-success") 
    AUDIO_SUCCESS.play();
    correctAnswerAmount++;     /* counts the correctly answered questions */
+   disableAnswers();
  }
+
+
+ function disableAnswers() {
+   for (let i = 1; i <= 4; i++) {
+     const answerNode = document.getElementById('answer_' + i);
+     answerNode.parentNode.style.setProperty('pointer-events', 'none');
+   }
+ }
+
+
+ function enableAnswers(){
+   for (let i = 1; i <= 4; i++) {
+      const answerNode = document.getElementById('answer_' + i);
+      answerNode.parentNode.style.setProperty('pointer-events', 'auto');
+    }
+ }
+ 
 
 
  function wrongAnswer(selection, idOfRightAnswer){
    document.getElementById(selection).parentNode.classList.add("bg-danger") 
    document.getElementById(idOfRightAnswer).parentNode.classList.add("bg-success") 
    AUDIO_FAIL.play();
+   disableAnswers();
  }
 
 
@@ -131,6 +151,7 @@ const questions = [
    } else {
       initNextQuestion();
    }
+   enableAnswers();
  }
 
 
@@ -183,32 +204,35 @@ function initNextQuestion(){
    correctAnswerAmount = 0;
    document.getElementById('card-right').innerHTML = quizTemplateHTML();
    init();
-
  }
 
 
  function quizTemplateHTML(){
-   return ` <h5 class="card-title" id="questionText">Frage</h5>
+   return ` <div class="category-buttons-mobile">
+               <a href="#" onclick="changeToHTML()">HTML</a>
+               <a href="#" onclick="changeToCSS()">CSS</a>
+            </div>
+            <h5 class="card-title" id="questionText">Frage</h5>
             <div class="question-container">
                <div class="card question-card mb-3" onclick="answer('answer_1')">
-               <div class="card-body" id="answer_1"></div>
+                  <div class="card-body" id="answer_1"></div>
                </div>
                <div class="card question-card mb-3" onclick="answer('answer_2')">
-               <div class="card-body" id="answer_2"></div>
+                  <div class="card-body" id="answer_2"></div>
                </div>
                <div class="card question-card mb-3" onclick="answer('answer_3')">
-               <div class="card-body" id="answer_3"></div>
+                  <div class="card-body" id="answer_3"></div>
                </div>
                <div class="card question-card mb-3" onclick="answer('answer_4')">
-               <div class="card-body" id="answer_4"></div>
+                  <div class="card-body" id="answer_4"></div>
                </div>
             </div>
             <div class="card-bottom">
                <div class="card-bottom-left">
-               <b id="questionNumber">1</b> von <b id="questionAmount">${questions.length}</b> Fragen
+                  <b id="questionNumber">1</b> von <b id="questionAmount">5</b> Fragen
                </div>
                <div class="card-bottom-right">
-               <button type="button" disabled id="next-button" onclick="disableButton(); nextQuestion();" class="btn btn-primary">Nächste Frage</button>
+                  <button type="button" disabled id="next-button" onclick="disableButton(); nextQuestion();" class="btn btn-primary">Nächste Frage</button>
                </div>
             </div>
    `;
@@ -223,3 +247,16 @@ function initNextQuestion(){
    }
  }
  
+
+ function changeToHTML(){
+   currentCategorie = 0;
+   resetGame();
+   showQuestion();
+ }
+
+
+ function changeToCSS(){
+   currentCategorie = 1;
+   resetGame();
+   showQuestion();
+ }
